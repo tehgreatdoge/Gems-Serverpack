2de7a2c5979e834a5ad495efdf1e65d7d90c7855
tehgreatdoge <61133736+tehgreatdoge@users.noreply.github.com>
2024-01-25 12:54:17 -0500

    basic ingredient reading doesn't support mekanism multi ingredients because im lazy

--- src/server_scripts/mekanism.js
+++ src/server_scripts/mekanism.js
@@ -1,4 +1,47 @@
 // priority: 9
+function parseMekanismIngredient(input) {
+    if (typeof(input) == "string") {
+        if (input.substring(0,1) == "#") {
+            return {
+                "ingredient": {
+                    "tag": input.substring(1),
+                },
+                "amount": 1
+            }
+        }
+        else {
+            return {
+                "ingredient": {
+                    "item": input
+                },
+                "amount": 1
+            }
+        }
+    }
+    if (typeof(input) == "object") {
+        if (input.ingredient) {
+            return input
+        }
+        else {
+            if (input.item) {
+                return {
+                    "ingredient": {
+                        "item": input.item
+                    },
+                    "amount": input.amount ?? 1
+                }
+            }
+            if (input.tag) {
+                return {
+                    "ingredient": {
+                        "tag": input.item
+                    },
+                    "amount": input.amount ?? 1
+                }
+            }
+        }
+    }
+}
 function registerChemicalDissolutionRecipe(event, output, gasInput, itemInput) {
     event.custom({
         type: "mekanism:dissolution",
